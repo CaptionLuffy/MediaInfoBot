17a3827dacbb3020e8b2b91a2a728d01164e78a1
CaptionLuffy <159707662+CaptionLuffy@users.noreply.github.com>
2024-06-19 07:59:11 +0530

    Update config.py

--- TelegramBot/config.py
+++ TelegramBot/config.py
@@ -8,5 +8,9 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
-SUDO_USERID = json.loads(getenv("SUDO_USERID"))
+OWNER_USERID = json.loads(getenv("OWNER_USERID"))
+SUDO_USERID = OWNER_USERID
+try:SUDO_USERID += json.loads(getenv("SUDO_USERID"))
+except:pass
+SUDO_USERID = list(set(SUDO_USERID))
 AUTHORIZED_CHATS = json.loads(getenv("AUTHORIZED_CHATS"))
